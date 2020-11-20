9b47d20b50f52ed8b3728b1a9c27667bfa5778be
TGauthier2011 <Tgauthier2011@gmail.com>
2020-11-20 11:27:28 -0800

    Responsive is better than fast

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,9 @@
-def primary():
-  print("Keep it logically awesome.")
+def main():
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  print(quotes[0])
 
-  #print(quotes)
-
-if __name__== "__primary__":
-  primary()
+if __name__== "__main__":
+  main()
